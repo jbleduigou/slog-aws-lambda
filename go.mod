7e77c7dfefbf75be53f4b9048c6f16360c736821
Jean-Baptiste Le Duigou <jb.leduigou@gmail.com>
2024-03-18 20:18:28 +0000

    ✅ adding unit tests

--- go.mod
+++ go.mod
@@ -2,4 +2,13 @@ module github.com/jbleduigou/slog-aws-lambda
 
 go 1.22.1
 
-require github.com/aws/aws-lambda-go v1.46.0
+require (
+	github.com/aws/aws-lambda-go v1.46.0
+	github.com/stretchr/testify v1.9.0
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ handler_test.go
@@ -0,0 +1,51 @@
+package slogawslambda
+
+import (
+	"log/slog"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetLogLevel(t *testing.T) {
+	tests := []struct {
+		name             string
+		envVariableValue string
+		want             slog.Leveler
+	}{
+		{
+			name:             "Should return Info given not set",
+			envVariableValue: "",
+			want:             slog.LevelInfo,
+		},
+		{
+			name:             "Should return Info given invalid value",
+			envVariableValue: "not-a-valid-value",
+			want:             slog.LevelInfo,
+		},
+		{
+			name:             "Should return Debug given lowercase debug",
+			envVariableValue: "debug",
+			want:             slog.LevelDebug,
+		},
+		{
+			name:             "Should return Warn given uppercase warn",
+			envVariableValue: "WARN",
+			want:             slog.LevelWarn,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if tt.envVariableValue != "" {
+				os.Setenv("LOG_LEVEL", tt.envVariableValue)
+			}
+
+			got := getLogLevel()
+
+			os.Unsetenv("LOG_LEVEL")
+
+			assert.Equal(t, got, tt.want)
+		})
+	}
+}
