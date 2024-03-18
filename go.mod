602dd174ce3dde0dccc7fb092e449b8e465cbdf2
Jean-Baptiste Le Duigou <jb.leduigou@gmail.com>
2024-03-18 20:14:09 +0000

    ✨ add the log handler for aws lambda functions

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/jbleduigou/slog-aws-lambda
 
 go 1.22.1
+
+require github.com/aws/aws-lambda-go v1.46.0
---
+++ handler.go
@@ -0,0 +1,53 @@
+package slogawslambda
+
+import (
+	"context"
+	"log/slog"
+	"os"
+
+	"github.com/aws/aws-lambda-go/lambdacontext"
+)
+
+type LambdaHandler struct {
+	slog.JSONHandler
+}
+
+func NewAWSLambdaHandler(ctx context.Context, opts *slog.HandlerOptions) slog.Handler {
+	if opts == nil {
+		opts = &slog.HandlerOptions{}
+	}
+
+	// Set the level based on the environment variable `LOG_LEVEL`
+	if opts.Level == nil {
+		opts.Level = getLogLevel()
+	}
+
+	// Retrieve AWS Request ID and lambda function arn
+	lc, _ := lambdacontext.FromContext(ctx)
+	requestID := lc.AwsRequestID
+	arn := lc.InvokedFunctionArn
+
+	// Create the Handler using the attributes from lambda context
+	return slog.NewJSONHandler(os.Stdout, opts).
+		WithAttrs([]slog.Attr{slog.String("function-arn", arn)}).
+		WithAttrs([]slog.Attr{slog.String("request-id", requestID)})
+}
+
+func getLogLevel() slog.Leveler {
+	str, found := os.LookupEnv("LOG_LEVEL")
+
+	// If no value is set, use Info as default Level
+	if !found {
+		return slog.LevelInfo
+	}
+
+	var l slog.Level
+	err := l.UnmarshalText([]byte(str))
+
+	// If invalid value is set, use Info as default Level
+	if err != nil {
+		return slog.LevelInfo
+	}
+
+	return l
+}
